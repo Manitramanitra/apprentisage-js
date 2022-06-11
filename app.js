8f81c2b53d9adef90f6e836e66b94f800571625a
Manitra Randriamanantena <randiluc@gmail.com>
2022-06-11 04:25:53 +0300

    fetch donne des utilisateurs

--- app.js
+++ app.js
@@ -1,42 +1,30 @@
+const loader = document.querySelector(".loader");
+const button = document.querySelector("button");
+let tab = new Array();
 
-// const contacts = [
-//     {
-//         id: 1,
-//         nom: 'Razafiarison',
-//         prenom: 'christian',
-//         email: 'christian@gmail.com',
-//         age: 12,
-//     },
-//     {
-//         id: 2,
-//         nom: 'Randriamanantena',
-//         prenom: 'manitra',
-//         email: 'manitra@gmail.com',
-//         age: 23,
-//     },
-//     {
-//         id: 3,
-//         nom:'Todisoa',
-//         prenom: 'mathamn',
-//         email: 'mathamn@gmail.com',
-//         age: 21,
-//     },
-//     {
-//         id: 4,
-//         nom: 'Ranaivoarison',
-//         prenom: 'harena',
-//         email: 'harena@gmail.com',
-//         age: 22,
-//     },
-// ];
-// const corpsDuTableau = document.querySelector('tbody');
+window.addEventListener('load',()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res=> res.json())
+        .then(data => tab = data)
+})
+function creeLigne(user){
+    return(
+    `<tr>
+        <td>${user.id}</td>
+        <td>${user.name}</td>
+        <td>${user.email}</td>
+        <td>${user.address.street}</td>
+        <td>${user.address.city}</td>
+        <td>${user.company.name}</td>
+    </tr>`)
+}
+button.addEventListener('click',()=>{
+    for(let tb of tab){
+        document.querySelector('tbody').innerHTML += creeLigne(tb);
+    }
+})
 
-// contacts.forEach( contact =>{
-//     const ligneContact = "<tr><td>"+ contact.id +"</td><td>"+ contact.nom +"</td><td>"+ contact.prenom +"</td><td>"+contact.email +"</td><td>"+ contact.age +"</td></tr>"
-//     corpsDuTableau.innerHTML += ligneContact;
-// })
-
-let n = 2222;
+/*let n = 2222; 
 let tab = new Array()
 let compteur=0
 
@@ -56,4 +44,4 @@ if(compteur == tab.length - 2){
     console.log(n+' est duodigit');
 }else{
     console.log(n+' tsy duodigit');
-}
+}*/
